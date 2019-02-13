9073cd174cff6f406957590472d2634bff8424f3
Michael <michale.ns32@gmail.com>
2019-02-13 03:32:58 +0200

    change db connect to mlab

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ var commentRoutes    = require('./routes/comments'),
     campgroundRoutes = require('./routes/campgrounds'),
     authRoutes       = require('./routes/index');
 
-mongoose.connect("mongodb://localhost:27017/yelp_camp", { useNewUrlParser: true });
+mongoose.connect("mongodb+srv://michalens32:<***REMOVED***>@cluster0-zrj43.mongodb.net/yelp_camp?retryWrites=true");
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", 'ejs');
 app.use(express.static(__dirname + "/public"));

a9eb6772f881eac07b62dbc3a6ca26f78591b332
Michael <michale.ns32@gmail.com>
2019-02-13 03:36:33 +0200

    change port for listening

--- app.js
+++ app.js
@@ -49,6 +49,6 @@ app.use(authRoutes);
 app.use("/campgrounds/:id/comments", commentRoutes);
 app.use("/campgrounds", campgroundRoutes);
 
-app.listen(3000, function(){
+app.listen(process.env.PORT || 3000, function(){
     console.log("Server started");
 });
